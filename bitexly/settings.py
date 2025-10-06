c09764fff05d0c6d265258aeebd3a225d1a44d71
O-GDev <105860521+O-GDev@users.noreply.github.com>
2025-10-06 23:24:51 +0100

    Update settings.py

--- bitexly/settings.py
+++ bitexly/settings.py
@@ -127,26 +127,26 @@ WSGI_APPLICATION = 'bitexly.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
+DATABASES = {
 #     # 'default': {
 #     #     'ENGINE': 'django.db.backends.sqlite3',
 #     #     'NAME': BASE_DIR / 'db.sqlite3',
 #     # }
-#       'default': dj_database_url.config(
-#         default=os.getenv('DATABASE_URL')
-#     )
-# }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bitexly_db',
-        'USER': 'admin',
-        'PASSWORD': 'Bitexly945',
-        'HOST': 'localhost',
-        'PORT': ''
-    }
+      'default': dj_database_url.config(
+       default=os.getenv('DATABASE_URL')
+    )
 }
+
+# DATABASES = {
+#    'default': {
+ #       'ENGINE': 'django.db.backends.postgresql',
+   #     'NAME': 'bitexly_db',
+  #      'USER': 'admin',
+  #      'PASSWORD': 'Bitexly945',
+  #      'HOST': 'localhost',
+    #    'PORT': ''
+ #   }
+#}
 
 
 # Password validation
