6e5e3b38ae71edb532d6a3aad9696c43bcd1df2a
Samuel <samuelonyeibor21@gmail.com>
2025-11-26 12:36:12 +0100

    update from email

--- bitexly/settings.py
+++ bitexly/settings.py
@@ -239,7 +239,7 @@ EMAIL_HOST = config("EMAIL_HOST")
 EMAIL_PORT = config("EMAIL_PORT", cast=int)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
+DEFAULT_FROM_EMAIL = "info@bitexly.com"
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
 
 # Meld Configuration
