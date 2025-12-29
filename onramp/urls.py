74754e47edbebedbe3dac53970fe5edf274812de
Samuel <116428301+Sam-3l@users.noreply.github.com>
2025-12-29 15:12:31 +0100

    Update urls.py

--- onramp/urls.py
+++ onramp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('quote/', views.get_onramp_quote, name='onramp_quote'),
+    path('quote/aah/', views.get_onramp_quote, name='onramp_quote'),
     path('payment-methods/', views.get_onramp_payment_methods, name='onramp_payment_methods'),
     path('generate-url/', views.generate_onramp_url, name='onramp_generate_url'),
     path('transaction-status/', views.get_onramp_transaction_status, name='onramp_transaction_status'),
